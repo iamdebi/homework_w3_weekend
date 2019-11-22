7133207f8f36f5c9a2c6f6191dfcf90ed8d3ff5e
Debbie <debiurquhart@live.co.uk>
2019-11-22 15:11:18 +0000

    adds content to seeds file

--- seeds.rb
+++ seeds.rb
@@ -1,1 +1,82 @@
+require('pry')
 require_relative('models/customer')
+require_relative('models/ticket')
+require_relative('models/film')
+
+customer1 = Customer.new(
+  {'name' = 'Debbie'
+  'funds' = 20
+  }
+)
+
+customer2 = Customer.new(
+  {'name' = 'Andrew'
+  'funds' = 50
+  }
+)
+
+cusotmer3 = Customer.new(
+  {'name' = 'Emma'
+  'funds' = 40
+  }
+)
+
+film1 = Film.new(
+  {'title' = 'Doctor Sleep'
+  'price' = 8
+  }
+)
+
+film2 = Film.new(
+  {'name' = 'frozen II'
+  'price' = 10
+  }
+)
+
+film3 = Film.new(
+  {'name' = 'Joker'
+  'price' = 9
+  }
+)
+
+ticket1 = Ticket.new(
+  {'cust_id' = customer1.id
+  'film_id' = film1.id
+  }
+)
+
+ticket2 = Ticket.new(
+  {'cust_id' = customer1.id
+  'film_id' = film2.id
+  }
+)
+
+ticket3 = Ticket.new(
+  {'cust_id' = customer2.id
+  'film_id' = film2.id
+  }
+)
+
+ticket4 = Ticket.new(
+  {'cust_id' = customer3.id
+  'film_id' = film1.id
+  }
+)
+
+ticket4 = Ticket.new(
+  {'cust_id' = customer3.id
+  'film_id' = film3.id
+  }
+)
+
+
+
+
+
+
+
+
+
+binding.pry
+
+nil
