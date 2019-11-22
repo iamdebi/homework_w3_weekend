33d35d2eaaea6122c8d5a279d60dad6e7cb6cfe8
Debbie <debiurquhart@live.co.uk>
2019-11-22 16:58:57 +0000

    adds find customer for film function

--- models/film.rb
+++ models/film.rb
@@ -27,5 +27,12 @@ class Film
     SqlRunner.run(sql, values)
   end
 
+  def customers()
+    sql = "SELECT customers.* FROM customers INNER JOIN tickets ON customers.id = cust_id WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    result = customers.map{|customer| Customer.new(customer)}
+  end
+
 
 end
